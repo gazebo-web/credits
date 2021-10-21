2dde5b781424f6d4be852e9fd05265f9d0c9e6ab
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-10-21 16:37:52 -0300

    Rename credits to service

--- pkg/application/service.go
+++ pkg/application/service.go
@@ -7,32 +7,32 @@ import (
 	"log"
 )
 
-// credits contains the business logic to manage credits.
-type credits struct {
+// service contains the business logic to manage service.
+type service struct {
 	logger *log.Logger
 }
 
-// IncreaseCredits increases the amount of credits for a given user.
-func (s *credits) IncreaseCredits(ctx context.Context, req api.IncreaseCreditsRequest) (api.IncreaseCreditsResponse, error) {
+// IncreaseCredits increases the amount of service for a given user.
+func (s *service) IncreaseCredits(ctx context.Context, req api.IncreaseCreditsRequest) (api.IncreaseCreditsResponse, error) {
 	panic("implement me")
 }
 
-// DecreaseCredits decreases the amount of credits for a given user.
-func (s *credits) DecreaseCredits(ctx context.Context, req api.DecreaseCreditsRequest) (api.DecreaseCreditsResponse, error) {
+// DecreaseCredits decreases the amount of service for a given user.
+func (s *service) DecreaseCredits(ctx context.Context, req api.DecreaseCreditsRequest) (api.DecreaseCreditsResponse, error) {
 	panic("implement me")
 }
 
-// GetBalance returns the current amount of credits of a given user.
-func (s *credits) GetBalance(ctx context.Context, req api.GetBalanceRequest) (api.GetBalanceResponse, error) {
+// GetBalance returns the current amount of service of a given user.
+func (s *service) GetBalance(ctx context.Context, req api.GetBalanceRequest) (api.GetBalanceResponse, error) {
 	panic("implement me")
 }
 
-// ConvertCurrency converts a certain amount of FIAT currency in USD to credits.
-func (s *credits) ConvertCurrency(ctx context.Context, req api.ConvertCurrencyRequest) (api.ConvertCurrencyResponse, error) {
+// ConvertCurrency converts a certain amount of FIAT currency in USD to service.
+func (s *service) ConvertCurrency(ctx context.Context, req api.ConvertCurrencyRequest) (api.ConvertCurrencyResponse, error) {
 	panic("implement me")
 }
 
-// Service holds the methods of the service in charge of managing user credits.
+// Service holds the methods of the service in charge of managing user service.
 type Service interface {
 	api.CreditsV1
 }
@@ -42,7 +42,7 @@ func NewService(logger *log.Logger) Service {
 	if logger == nil {
 		logger = log.New(io.Discard, "", log.LstdFlags)
 	}
-	return &credits{
+	return &service{
 		logger: logger,
 	}
 }
